02f641324b39cca329286741dfd37f8841c125ad
Kira Luethe <ksluethe@mail.fhsu.edu>
2022-02-04 21:48:17 -0800

    Assignment 1

--- src/App.js
+++ src/App.js
@@ -2,22 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const toDoList = [
+    {
+      id: 1,
+      title: "check new homework assignments"
+    },
+    {
+      id: 2,
+      title: "read next chapter of textbook"
+    },
+    {
+      id: 3,
+      title: "watch lecture video"
+    }
+  ];
+  const myName = "Kira"; 
+  const randomTask = () => {
+    const index = Math.floor(Math.random() * 3);
+    return toDoList[index].title;
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p> Hello <b>{myName}</b>, you have a task <b className = "random">{randomTask()}</b> to finish.</p>
     </div>
   );
 }
